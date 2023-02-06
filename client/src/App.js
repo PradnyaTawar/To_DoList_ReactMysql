2c83274a6dbd83e42735c557f16a89a95d7425e0
PradnyaTawar <tawar.rani96@gmail.com>
2023-02-06 17:10:02 +0530

    create react app client

---
+++ client/src/App.js
@@ -0,0 +1,15 @@
+import ToDoApp from './to-do';
+import React from 'react';
+import { BrowserRouter,Route, Routes } from "react-router-dom";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<ToDoApp />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
